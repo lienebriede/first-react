793a73059be6cd34920432020332a28a31a1effa
Liene Briede <lienebriede84@gmail.com>
2024-07-17 12:54:21 +0000

    Add functional component

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalGreeting from './components/FunctionalGreeting';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32" />
+    </div>
+  );
+}
+
+export default App;
