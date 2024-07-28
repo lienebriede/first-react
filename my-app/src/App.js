ceb849dcf3ca646563395d0b031a8e9d3a34b74f
Liene Briede <lienebriede84@gmail.com>
2024-07-28 16:08:02 +0000

    State hook with array, state hook with obejcts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 
 function App() {
   return <div className="App">
-    <ControlledFormHooks />
+    <UseStateWithObjects />
   </div>;
 }
 
