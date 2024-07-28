7c2a38cc4679132e809f189155f25181739b5526
Liene Briede <lienebriede84@gmail.com>
2024-07-28 10:17:34 +0000

    Add form with refs, searchbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import ControlledForm from './components/ControlledForm';
+import SearchBar from './components/SearchBar';
 
 
 function App() {
   return <div className="App">
-    <ControlledForm />
+    <SearchBar />
   </div>;
 }
 
