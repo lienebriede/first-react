8c28d363a1e58267c53cbe7b24389c9ccae1b1f9
Liene Briede <lienebriede84@gmail.com>
2024-07-28 12:18:31 +0000

    Class counter vs hooks counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import SearchBar from './components/SearchBar';
+import HooksCounter from './components/HooksCounter';
 
 
 function App() {
   return <div className="App">
-    <SearchBar />
+    <HooksCounter />
   </div>;
 }
 
